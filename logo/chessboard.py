0fc5f08dc250ad80e29b25e6f0b57652d15f7d48
louisehoa <louisehoatruong@hotmail.co.uk>
2014-08-19 12:10:29 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -1,7 +1,7 @@
 #from tealight.logo import (move, turn,
-                           pen_down, pen_up,
-                           show_turtle, hide_turtle,
-                           color, speed)
+                           #pen_down, pen_up,
+                           #show_turtle, hide_turtle,
+                           #color, speed)
 
 #def square(side):
   #for i in range(0,4):
