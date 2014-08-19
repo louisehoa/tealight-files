b2e83b5b85d3fbd808140bd6b35cff333b963cc8
louisehoa <louisehoatruong@hotmail.co.uk>
2014-08-19 12:10:17 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -1,15 +1,28 @@
-from tealight.logo import (move, turn,
+#from tealight.logo import (move, turn,
                            pen_down, pen_up,
                            show_turtle, hide_turtle,
                            color, speed)
 
-def square(side):
-  for i in range(0,4):
-    move(side)
-    turn(90)
+#def square(side):
+  #for i in range(0,4):
+    #move(side)
+    #turn(90)
 
-def drawboard():
-   for count in range(0,63):
-      square(50 + (count * 10))
+#def drawboard():
+   #for count in range(0,63):
+      #square(50 + (count * 10))
 
-drawboard()
+#drawboard()
+
+from tealight.logo import move, turn
+
+def spiral(size):
+  
+  if size > 300:
+    return
+  
+  move(size)
+  turn(90)
+  spiral(size + 5)
+  
+spiral(0)
