cbb5cf2f8bac24c966fad19b5694571f656f378f
louisehoa <louisehoatruong@hotmail.co.uk>
2014-08-19 11:57:19 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -1,1 +1,4 @@
-print "A new file!"
+from tealight.logo import (move, turn,
+                           pen_down, pen_up,
+                           show_turtle, hide_turtle,
+                           color, speed)
