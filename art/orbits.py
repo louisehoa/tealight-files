257a4435c8a1cbaf785ee12a19ddf975e60cbb8b
louisehoa <louisehoatruong@hotmail.co.uk>
2014-08-20 11:51:26 +0100

    Running art/orbits.py

--- art/orbits.py
+++ art/orbits.py
@@ -1,4 +1,4 @@
-from tealight.art import (color, line, spot, circle, box, image, text, background)
+from tealight.art import (color, line, spot, circle, box, image, text, background, gravity, friction)
 
 x = 600
 y = 400
