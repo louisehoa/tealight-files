67ff2081e5677fe94ab9c258d1cf76b833a65f53
louisehoa <louisehoatruong@hotmail.co.uk>
2014-08-21 15:16:40 +0100

    Running art/Dinosaurs.py

--- art/Dinosaurs.py
+++ art/Dinosaurs.py
@@ -1,4 +1,5 @@
 from tealight.net import(connect, send)
+from tealight.art import (color, line, spot, circle, box, image, text, background)
 
 connect ('Dinosaurs.py')
 message = image("http://i.imgur.com/0zoXV5h.png")
