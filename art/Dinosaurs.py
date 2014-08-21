bef340002185b5fd373560561795c3f3ac074e66
louisehoa <louisehoatruong@hotmail.co.uk>
2014-08-21 15:17:33 +0100

    Running art/Dinosaurs.py

--- art/Dinosaurs.py
+++ art/Dinosaurs.py
@@ -2,7 +2,7 @@ from tealight.net import(connect, send)
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
 connect ('Dinosaurs.py')
-message = image("http://i.imgur.com/0zoXV5h.png")
+message = image(200, 100, "http://i.imgur.com/0zoXV5h.png")
 send(message)
 
 def handle_message(message):
