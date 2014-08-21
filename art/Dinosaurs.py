0e5b3247f908342cf60d2c48118cb59368f5a7f2
louisehoa <louisehoatruong@hotmail.co.uk>
2014-08-21 15:06:18 +0100

    Running art/Dinosaurs.py

--- art/Dinosaurs.py
+++ art/Dinosaurs.py
@@ -1,8 +1,8 @@
 from tealight.net import(connect, send)
 
 connect ('Dinosaurs.py')
-message = str(input())
+message = input([Prompt])
 send(message)
 
 def handle_message(message):
-  print "Received message: " + message
+  print "Received message: " + str(message)
