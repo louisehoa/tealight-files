8729fee331915e3ca157f063367d4cb2b1007272
louisehoa <louisehoatruong@hotmail.co.uk>
2014-08-22 08:55:55 +0100

    Running art/Dinosaurs.py

--- art/Dinosaurs.py
+++ art/Dinosaurs.py
@@ -1,9 +1,7 @@
 from tealight.net import(connect, send)
-from tealight.art import (color, line, spot, circle, box, image, text, background)
 
 connect ('Dinosaurs.py')
-message = image(200, 100, "http://i.imgur.com/0zoXV5h.png")
-send(message)
+message = 'Meerkat'
 
 def handle_message(message):
   print "Received message: " + message
